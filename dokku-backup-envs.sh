32bc71ba39b4e7603eb2942d1cd88ac28d783eb5
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2020-12-16 16:37:12 -0300

    try get dokku hostname

--- dokku-backup-envs.sh
+++ dokku-backup-envs.sh
@@ -3,9 +3,10 @@
 aws_region=${AWS_REGION:-us-east-1}
 bucket_name=${BUCKET_NAME}
 folder_date=$(date '+%Y%m%d_%H%M')
-hostname=$HOSTNAME
-
 dokku_dir='/home/dokku'
+
+dokku_hostname=`cat $dokku_dir/HOSTNAME`
+hostname=${dokku_hostname:-$HOSTNAME}
 app_names=`ls $dokku_dir`
 
 if [ -z "$bucket_name" ]; then
