a2338312a2a171c1f54e60fc0de2b4be54942221
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2020-12-16 16:36:31 -0300

    add awscli var

--- dokku-backup-envs.sh
+++ dokku-backup-envs.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 aws_region=${AWS_REGION:-us-east-1}
 bucket_name=${BUCKET_NAME}
 folder_date=$(date '+%Y%m%d_%H%M')
@@ -11,12 +13,14 @@ if [ -z "$bucket_name" ]; then
   exit 1
 fi
 
+awscli=`which aws`
+
 for entry in $app_names
 do
   env_file=$dokku_dir/$entry/ENV
 
   if [ -f "$env_file" ]; then
-    AWS_REGION=$aws_region aws s3 cp $env_file s3://$bucket_name/$folder_date/$hostname/$entry/ENV
+    AWS_REGION=$aws_region $awscli s3 cp $env_file s3://$bucket_name/$folder_date/$hostname/$entry/ENV
   fi
 done
 
