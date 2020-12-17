f22787a73f1dfdfc7de2aa005d2320c52f3b159a
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2020-12-17 09:35:24 -0300

    fixed when dokku hostname is blank

--- dokku-backup-envs.sh
+++ dokku-backup-envs.sh
@@ -5,10 +5,16 @@ aws_region=${AWS_REGION:-us-east-1}
 bucket_name=${BUCKET_NAME}
 folder_date=$(date '+%Y%m%d_%H%M')
 dokku_dir='/home/dokku'
+app_names=`ls $dokku_dir`
 
 dokku_hostname=`cat $dokku_dir/HOSTNAME`
-hostname=${dokku_hostname:-$HOSTNAME}
-app_names=`ls $dokku_dir`
+dokku_hostname_length=${#dokku_hostname}
+
+if [ $dokku_hostname_length -gt 0 ]; then
+  host_name=$dokku_hostname
+else
+  host_name=`hostname`
+fi
 
 if [ -z "$bucket_name" ]; then
   echo "env BUCKET_NAME cannot be blank"
@@ -28,7 +34,7 @@ do
   fi
 
   if [ -f "$env_file" ]; then
-    AWS_REGION=$aws_region $awscli s3 cp $env_file s3://$bucket_name/$folder_date/$hostname/$env_file_s3
+    AWS_REGION=$aws_region $awscli s3 cp $env_file s3://$bucket_name/$folder_date/$host_name/$env_file_s3
   fi
 done
 
