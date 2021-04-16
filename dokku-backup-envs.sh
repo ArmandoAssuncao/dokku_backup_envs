354d40a962c5e6da874427869dd50cfa91816e14
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2021-04-15 22:32:21 -0300

    fixed shellcheck warnings

--- dokku-backup-envs.sh
+++ dokku-backup-envs.sh
@@ -2,18 +2,18 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 aws_region=${AWS_REGION:-us-east-1}
-bucket_name=${BUCKET_NAME}
-folder_date=$(date '+%Y%m%d_%H%M')
-dokku_dir='/home/dokku'
-app_names=`ls $dokku_dir`
+bucket_name=$BUCKET_NAME
+folder_date=$(date "+%Y%m%d_%H%M")
+dokku_dir="/home/dokku"
+app_names=$(ls $dokku_dir)
 
-dokku_hostname=`cat $dokku_dir/HOSTNAME`
+dokku_hostname=$(cat $dokku_dir/HOSTNAME)
 dokku_hostname_length=${#dokku_hostname}
 
-if [ $dokku_hostname_length -gt 0 ]; then
+if [ "$dokku_hostname_length" -gt 0 ]; then
   host_name=$dokku_hostname
 else
-  host_name=`hostname`
+  host_name=$(hostname)
 fi
 
 if [ -z "$bucket_name" ]; then
@@ -21,11 +21,11 @@ if [ -z "$bucket_name" ]; then
   exit 1
 fi
 
-awscli=`which aws`
+awscli=$(which aws)
 
 for entry in $app_names
 do
-  if [ $entry = 'ENV' ]; then
+  if [ "$entry" = "ENV" ]; then
     env_file=$dokku_dir/ENV # global ENV
     env_file_s3=ENV
   else
@@ -34,7 +34,7 @@ do
   fi
 
   if [ -f "$env_file" ]; then
-    AWS_REGION=$aws_region $awscli s3 cp $env_file s3://$bucket_name/$folder_date/$host_name/$env_file_s3
+    AWS_REGION=$aws_region $awscli s3 cp "$env_file" "s3://$bucket_name/$folder_date/$host_name/$env_file_s3"
   fi
 done
 
